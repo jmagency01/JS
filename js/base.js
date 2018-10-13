9125d06d50cb0d7b888b0a29e29af6deb825a7ee
jmagency <jmagency@mail.ru>
2018-10-13 10:08:17 +0300

    12.10.2018

---
+++ js/base.js
@@ -0,0 +1,39 @@
+//задача №1
+/*let a = Math.random() * 10;
+let b = Math.random() * 10;
+let c = Math.random() * 10;
+var S = a*b*c;
+console.log(parseInt(a));
+console.log(parseInt(c));
+console.log(a>c);
+*/
+
+//задача №2
+/*let a, b = 1000;
+var c = 15;
+var d = 25;
+var f = b/d;
+console.log(f);
+var G = a -(c*f);
+console.log(G);*/
+//результат должен быть 400м. но выдает NuN
+
+//задача №3
+/*var a = 15*10
+var b = 600
+var S = b-a;
+console.log(S);*/
+//задача №4
+let x = 11;
+let y =9;
+let res = x > y ? x--: y;
+console.log(res);
+
+//Задача №5
+/*var m = Math.random() * 9;
+var n = Math.random() * 9;
+console.log(n); console.log(m);
+while (parseInt(m) < parseInt(n)) {
+    alert(n);
+    alert(m);
+}*/
