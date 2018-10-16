6699af5c8fa850c4ae9934d2f1fb3635ae6d4d40
jmagency <jmagency@mail.ru>
2018-10-16 15:02:35 +0300

    Пацан исправился)

--- js/base.js
+++ js/base.js
@@ -1,39 +1,46 @@
 //задача №1
-/*let a = Math.random() * 10;
-let b = Math.random() * 10;
-let c = Math.random() * 10;
-var S = a*b*c;
-console.log(parseInt(a));
-console.log(parseInt(c));
-console.log(a>c);
+/*
+let shir = 12; //ширина
+let dlina = 15; //длина
+let visota = 16; // высота
+var S = 2*((shir*dline)+(dlina*visota)+(shir*visota));
+
+if (shir>visota){alert('Ширина больше высоты.' + 'Площадь: ' + S);
+}
+else {alert('Высота больше ширины.'+ 'Площадь: ' + S);}
 */
 
+
+
 //задача №2
-/*let a, b = 1000;
-var c = 15;
-var d = 25;
-var f = b/d;
-console.log(f);
-var G = a -(c*f);
-console.log(G);*/
+let hr =10*100;
+let d = '15';
+let sh = '25';
+var f = hr % (d*sh);
+console.log('Остальсь свободно ' + f + ' квадратных метров');
 //результат должен быть 400м. но выдает NuN
 
 //задача №3
-/*var a = 15*10
+var a = 15*100
 var b = 600
-var S = b-a;
-console.log(S);*/
+var R = b-a;
+console.log(R);
 //задача №4
 let x = 11;
 let y =9;
-let res = x > y ? x--: y;
-console.log(res);
+while (x>=y) {
+    x--; y++;
+}
+console.log('x = '+ x, 'y = ' + y);
+
+
 
 //Задача №5
-/*var m = Math.random() * 9;
-var n = Math.random() * 9;
-console.log(n); console.log(m);
-while (parseInt(m) < parseInt(n)) {
-    alert(n);
-    alert(m);
-}*/
+let m = prompt('Введите M');
+let n = prompt('Введите N');
+n = parseFloat(n);
+m = parseFloat(m);
+10 - m > 10 - (-n) ? console.log(m + ' ближе к 10, чем ' + n) :console.log(n + ' ближе к 10, чем ' + m);
+
+//Задача №6
+
